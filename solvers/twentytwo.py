51fdfeac04f948283e57f4ab4e4eca9062463615
Frank van Woerden <F.M.vanWoerden@nvwa.nl>
2023-01-17 10:41:54 +0100

    moved day 3 item scores to helper file

--- solvers/twentytwo.py
+++ solvers/twentytwo.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from helpers import helper_functions as hf
+from helpers import helper_values as hv
 
 
 def day_1_1(input_data):
@@ -49,60 +49,10 @@ def day_3_1(data):
     data["comp1"] = data.apply(lambda row: row.pack[:len(row.pack) // 2], axis=1)
     data["comp2"] = data.apply(lambda row: row.pack[len(row.pack) // 2:], axis=1)
     data["shared"] = data.apply(lambda row: set(row.comp1).intersection(set(row.comp2)).pop(), axis=1)
-    data["priority_score"] = data["shared"].replace({'a': 1,
-                                                     'b': 2,
-                                                     'c': 3,
-                                                     'd': 4,
-                                                     'e': 5,
-                                                     'f': 6,
-                                                     'g': 7,
-                                                     'h': 8,
-                                                     'i': 9,
-                                                     'j': 10,
-                                                     'k': 11,
-                                                     'l': 12,
-                                                     'm': 13,
-                                                     'n': 14,
-                                                     'o': 15,
-                                                     'p': 16,
-                                                     'q': 17,
-                                                     'r': 18,
-                                                     's': 19,
-                                                     't': 20,
-                                                     'u': 21,
-                                                     'v': 22,
-                                                     'w': 23,
-                                                     'x': 24,
-                                                     'y': 25,
-                                                     'z': 26,
-                                                     'A': 27,
-                                                     'B': 28,
-                                                     'C': 29,
-                                                     'D': 30,
-                                                     'E': 31,
-                                                     'F': 32,
-                                                     'G': 33,
-                                                     'H': 34,
-                                                     'I': 35,
-                                                     'J': 36,
-                                                     'K': 37,
-                                                     'L': 38,
-                                                     'M': 39,
-                                                     'N': 40,
-                                                     'O': 41,
-                                                     'P': 42,
-                                                     'Q': 43,
-                                                     'R': 44,
-                                                     'S': 45,
-                                                     'T': 46,
-                                                     'U': 47,
-                                                     'V': 48,
-                                                     'W': 49,
-                                                     'X': 50,
-                                                     'Y': 51,
-                                                     'Z': 52})
+    data["priority_score"] = data["shared"].replace(hv.item_scores)
 
     return data["priority_score"].sum()
+
 
 def day_3_2(data):
     g = data.groupby(data.index // 3)
@@ -113,57 +63,6 @@ def day_3_2(data):
     })
 
     groups['badge'] = groups.apply(lambda row: set(row[0]).intersection(set(row[1]), set(row[2])).pop(), axis=1)
-    groups['badge_score'] = groups['badge'].replace({'a': 1,
-                                                     'b': 2,
-                                                     'c': 3,
-                                                     'd': 4,
-                                                     'e': 5,
-                                                     'f': 6,
-                                                     'g': 7,
-                                                     'h': 8,
-                                                     'i': 9,
-                                                     'j': 10,
-                                                     'k': 11,
-                                                     'l': 12,
-                                                     'm': 13,
-                                                     'n': 14,
-                                                     'o': 15,
-                                                     'p': 16,
-                                                     'q': 17,
-                                                     'r': 18,
-                                                     's': 19,
-                                                     't': 20,
-                                                     'u': 21,
-                                                     'v': 22,
-                                                     'w': 23,
-                                                     'x': 24,
-                                                     'y': 25,
-                                                     'z': 26,
-                                                     'A': 27,
-                                                     'B': 28,
-                                                     'C': 29,
-                                                     'D': 30,
-                                                     'E': 31,
-                                                     'F': 32,
-                                                     'G': 33,
-                                                     'H': 34,
-                                                     'I': 35,
-                                                     'J': 36,
-                                                     'K': 37,
-                                                     'L': 38,
-                                                     'M': 39,
-                                                     'N': 40,
-                                                     'O': 41,
-                                                     'P': 42,
-                                                     'Q': 43,
-                                                     'R': 44,
-                                                     'S': 45,
-                                                     'T': 46,
-                                                     'U': 47,
-                                                     'V': 48,
-                                                     'W': 49,
-                                                     'X': 50,
-                                                     'Y': 51,
-                                                     'Z': 52})
+    groups['badge_score'] = groups['badge'].replace(hv.item_scores)
 
     return groups['badge_score'].sum()
