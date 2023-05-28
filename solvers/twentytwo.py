08f626d9c16984d6d01dfa13739b6905e1e42c6e
Nedreow <F.vanwoerden23@gmail.com>
2023-05-28 11:10:34 +0200

    Finished day 3

--- solvers/twentytwo.py
+++ solvers/twentytwo.py
@@ -49,6 +49,19 @@ def day_3_1(data):
     data["comp1"] = data.apply(lambda row: row.pack[:len(row.pack) // 2], axis=1)
     data["comp2"] = data.apply(lambda row: row.pack[len(row.pack) // 2:], axis=1)
     data["shared"] = data.apply(lambda row: set(row.comp1).intersection(set(row.comp2)).pop(), axis=1)
+    data["priority"] = data.apply(lambda row: ord(row.shared) - 38 if row.shared.isupper() else ord(row.shared) - 96, axis=1)
 
-    return data["shared"][1]
+    return data.priority.sum()
 
+
+def day_3_2(data):
+    data.rename(columns={0: "pack"}, inplace=True)
+    groups = pd.DataFrame({
+        "pack0": (data.iloc[::3, 0]).reset_index(drop=True),
+        "pack1": (data.iloc[1::3, 0]).reset_index(drop=True),
+        "pack2": (data.iloc[2::3, 0]).reset_index(drop=True)}
+    )
+    groups["badge"] = groups.apply(lambda row: set(row.pack0).intersection(row.pack1, row.pack2).pop(), axis=1)
+    groups["priority"] = groups.apply(lambda row: ord(row.badge) - 38 if row.badge.isupper() else ord(row.badge) - 96, axis=1)
+
+    return groups.priority.sum()
