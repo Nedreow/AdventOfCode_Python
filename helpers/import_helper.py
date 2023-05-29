67a4f5f60c237e0ba762686d4a408e92756e7b31
Nedreow <F.vanwoerden23@gmail.com>
2023-05-29 13:12:20 +0200

    Use python engine in read_csv

--- helpers/import_helper.py
+++ helpers/import_helper.py
@@ -2,5 +2,10 @@ import pandas as pd
 
 
 def import_input(year, day, sep=" "):
-    return pd.read_csv("input_files/{}/{}.txt".format(year, day), sep=sep, header=None, skip_blank_lines=False)
-
+    return pd.read_csv(
+        "input_files/{}/{}.txt".format(year, day),
+        sep=sep,
+        header=None,
+        skip_blank_lines=False,
+        engine='python'
+    )
